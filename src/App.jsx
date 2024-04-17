2ede1d2b254cdd8c578f36ff98d2b58df250d0d4
Samir <sobiroff23@gmail.com>
2024-04-17 16:27:31 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <header>
+        <div>
+        <img src="" alt="" />
+
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default App;
