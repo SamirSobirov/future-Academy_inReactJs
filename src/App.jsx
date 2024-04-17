d8703474cd05eccaf3301c2655c205e90452f9b1
Samir <sobiroff23@gmail.com>
2024-04-17 17:14:55 +0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import Header from './header.jsx'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <Header/> 
--- src/header.jsx
+++ src/header.jsx
@@ -1,18 +1,25 @@
-import logo from "./assets/header_logo.svg"
+import header_Logo from "./assets/header_logo.svg";
+import header_logo_mini_burger_menu from "./assets/header_mini_burger_menu.svg";
 
 function Header() {
-    
-  
-    return (
-      <>
-        <header>
-          <div>
-          <img src={logo} alt="" />
+  return (
+    <div className="header">
+      <header>
+        <img className="header_logo" src={header_Logo} alt="" />
 
-          </div>
-        </header>
-      </>
-    );
-  }
+        
+        
+          <img
+            className="header_logo_mini_burger_menu"
+            src={header_logo_mini_burger_menu}
+            alt=""
+          />
+          <p className="header_p1">Все курсы</p>
 
-  export default Header 
+       
+      </header>
+    </div>
+  );
+}
+
+export default Header;
