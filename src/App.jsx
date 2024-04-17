1d6cd32b0c9adc358f933a1e0c9ea6e779054ea6
Samir <sobiroff23@gmail.com>
2024-04-17 20:49:58 +0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './header.jsx'
-
-
+import { useState } from "react";
+import "./App.css";
+import Header from "./header.jsx";
+import Asides from "./aside.jsx";
 
 function App() {
-  
   return (
     <>
-      <Header/> 
+      <Header />
+
+      <Asides />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/aside.jsx
@@ -0,0 +1,15 @@
+function Asides() {
+  return (
+    <main>
+      <div>
+        
+
+        <p className="main_p">Все программы обучения</p>
+
+
+      </div>
+    </main>
+  );
+}
+
+export default Asides;
