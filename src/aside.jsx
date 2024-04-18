e92a6b94660097898c23785c4ab1edb1dfb5f3b2
Samir <sobiroff23@gmail.com>
2024-04-18 14:41:59 +0500

    fixed

--- src/aside.jsx
+++ src/aside.jsx
@@ -1,13 +1,24 @@
+import children_ageSelection from "./assets/main__ageSelection.svg"
+
 function Asides() {
   return (
     <main>
-      <div>
+      <div className="main_text">
         
 
         <p className="main_p">Все программы обучения</p>
 
 
+      <div className="age_selection">
+        
+        <button btn_age_sec>
+            <img src={children_ageSelection} alt="" />
+            <p><b>Дети</b></p>
+        </button>
       </div>
+
+      </div>
+
     </main>
   );
 }
